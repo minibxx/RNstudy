7126d1f3dab99f1bdfd5c01cd856b4e490b4ac08
minibxx <alsdbqls1004@gmail.com>
2025-02-13 18:12:19 +0900

    erp first commit

---
+++ erp/src/app/page.tsx
@@ -0,0 +1,10 @@
+import Organization from "@/components/Organization";
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <>
+      <Organization/>
+    </>
+  )
+}
---
+++ erp/src/components/Organization.tsx
@@ -0,0 +1,143 @@
+import React from 'react'
+
+function Organization() {
+    return (
+        <ul>
+            <li>
+                <li>
+                    <details>
+                            <summary>📂 비즈비 (118)</summary>
+                            <ul>
+                                <li></li>
+                            </ul>
+                        </details>
+                    </li>
+
+                <ul className='ml-[20px]'>
+                    <li>  의장 (1)</li>
+                    <li>  대표 (1)</li>
+                    <li>
+                        <details>
+                            <summary>솔루션사업본부 (1)</summary>
+                            <ul>
+                                <li></li>
+                            </ul>
+                        </details>
+                    </li>
+
+                    <li>
+                        <details>
+                            <summary>ERP1본부 (10)</summary>
+                            <ul>
+                                <li></li>
+                            </ul>
+                        </details>
+                    </li>
+
+                    <li>
+                        <details>
+                            <summary>ERP2본부 (20)</summary>
+                            <ul>
+                                <li></li>
+                            </ul>
+                        </details>
+                    </li>
+
+                    <li>
+                        <details>
+                            <summary>솔루션1본부 (21)</summary>
+                            <ul>
+                                <li></li>
+                            </ul>
+                        </details>
+                    </li>
+
+                    <li>
+                        <details>
+                            <summary>솔루션2본부 (23)</summary>
+                            <ul>
+                                <li></li>
+                            </ul>
+                        </details>
+                    </li>
+
+                    <li>
+                        <details>
+                            <summary>솔루션기획팀 (4)</summary>
+                            <ul>
+                                <li></li>
+                            </ul>
+                        </details>
+                    </li>
+
+                    <li>
+                        <details>
+                            <summary>UI/UX팀 (7)</summary>
+                            <ul>
+                                <li></li>
+                            </ul>
+                        </details>
+                    </li>
+
+                    <li>
+                        <details>
+                            <summary>WORKS팀 (8)</summary>
+                            <ul>
+                                <li></li>
+                            </ul>
+                        </details>
+                    </li>
+
+                    <li>
+                        <details>
+                            <summary>솔루션4팀 (4)</summary>
+                            <ul>
+                                <li></li>
+                            </ul>
+                        </details>
+                    </li>
+
+                    <li>
+                        <details>
+                            <summary>기술본부 (5)</summary>
+                            <ul>
+                                <li></li>
+                            </ul>
+                        </details>
+                    </li>
+
+                    <li>
+                        <details>
+                            <summary>기술연구소 (10)</summary>
+                            <ul>
+                                <li></li>
+                            </ul>
+                        </details>
+                    </li>
+
+                    <li>
+                        <details>
+                            <summary>총무팀 (1)</summary>
+                            <ul>
+                                <li></li>
+                            </ul>
+                        </details>
+                    </li>
+
+                    <li>
+                        <details>
+                            <summary>프로젝트그룹 (16)</summary>
+                            <ul>
+                                <li></li>
+                            </ul>
+                        </details>
+                    </li>
+
+                </ul>
+            </li>
+        </ul>
+
+    )
+}
+
+export default Organization
