83465a4d389bb8bc040f88cfbcfbf52f8e68b2d6
minibxx <alsdbqls1004@gmail.com>
2025-02-19 17:10:39 +0900

    월 달력 배열 코드 훅 분리 및 리팩토링

--- erp/src/components/calendar/DatePickCalender.tsx
+++ erp/src/components/calendar/DatePickCalender.tsx
@@ -1,13 +1,15 @@
-"use client"
-import dayjs, { Dayjs } from 'dayjs';
-import React, { useState } from 'react';
-import Image from 'next/image';
-import styled from 'styled-components';
-import weekOfYear from 'dayjs/plugin/weekOfYear';
-import weekday from 'dayjs/plugin/weekday';
-import { useCalendarStore } from '@/stores/DatePickCalendarStore';
+"use client";
 
-const DaysOfWeek = ['일', '월', '화', '수', '목', '금', '토'];
+import dayjs, { Dayjs } from "dayjs";
+import React from "react";
+import Image from "next/image";
+import styled from "styled-components";
+import weekOfYear from "dayjs/plugin/weekOfYear";
+import weekday from "dayjs/plugin/weekday";
+import { useCalendarStore } from "@/stores/DatePickCalendarStore";
+import { useCalendarArray } from "@/hooks/useCalendarArray"; 
+
+const DaysOfWeek = ["일", "월", "화", "수", "목", "금", "토"];
 
 const CalendarContainer = styled.div`
   width: 15vw;
@@ -29,84 +31,72 @@ dayjs.extend(weekOfYear);
 dayjs.extend(weekday);
 
 function DatePickCalendar({ onChange }: { onChange: (value: string) => void }) {
-  const { currentMonth, setCurrentDate, startDate, endDate, selectDate, selectedDate } = useCalendarStore();
+  const { currentMonth, setCurrentDate, selectDate, selectedDate } = useCalendarStore();
+  
+  const calendar = useCalendarArray(currentMonth); 
 
-  let day: Dayjs = startDate.subtract(1, 'day'); // 시작 전날 설정
-  const calendar: Dayjs[][] = [];
-
-  while (day.isBefore(endDate, 'day')) {
-    calendar.push(
-      Array(7)
-        .fill(null)
-        .map(() => {
-          day = day.add(1, 'day');
-          return day;
-        })
-    );
-  }
-
-	const handleDateClick = (date: Dayjs) => {
+  const handleDateClick = (date: Dayjs) => {
     selectDate(date);
-    const formattedDate = date.format('YYYY-MM-DD');
-    onChange(formattedDate); // 부모로 선택된 날짜 전달
+    const formattedDate = date.format("YYYY-MM-DD");
+    onChange(formattedDate);
   };
 
   return (
-    <>
-      <CalendarContainer>
-        <div className='flex justify-center w-[100%]'>
-          <Image 
-            alt="previous month" 
-            src="/BIZ.png" 
-            width={20} 
-            height={20} 
-            onClick={() => setCurrentDate(currentMonth.subtract(1, 'month'))}
-            className="cursor-pointer"
-          />
-          <div>{currentMonth.format('MM월 YYYY')}</div>
-          <Image 
-            alt="next month" 
-            src="/BIZ.png" 
-            width={20} 
-            height={20} 
-            onClick={() => setCurrentDate(currentMonth.add(1, 'month'))}
-            className="cursor-pointer"
-          />
-        </div>
-        <CalendarDiv>
-          {DaysOfWeek.map((day, index) => (
-            <span 
-              style={{ fontSize: '14px', padding: '8px', width: '5%', textAlign: 'center' }} 
-              key={index}
-            >
-              {day}
-            </span>
-          ))}
-        </CalendarDiv>
-        <div>
-          {calendar.map((week, weekIndex) => (
-            <CalendarDiv key={weekIndex}>
-              {week.map((date, dayIndex) => (
-                <div 
-                  key={dayIndex} 
-                  style={{
-                    fontSize: '14px', 
-                    padding: '8px', 
-                    width: '5%', 
-                    textAlign: 'center', 
-                    cursor: 'pointer',
-                    backgroundColor: selectedDate?.isSame(date, 'day') ? '#cce4ff' : 'transparent',
-                  }}
-                  onClick={() => handleDateClick(date)} 
-                >
-                  {date.format('D')}
-                </div>
-              ))}
-            </CalendarDiv>
-          ))}
-        </div>
-      </CalendarContainer>
-    </>
+    <CalendarContainer>
+      <div className="flex justify-center w-[100%]">
+        <Image
+          alt="previous month"
+          src="/BIZ.png"
+          width={20}
+          height={20}
+          onClick={() => setCurrentDate(currentMonth.subtract(1, "month"))}
+          className="cursor-pointer"
+        />
+        <div>{currentMonth.format("MM월 YYYY")}</div>
+        <Image
+          alt="next month"
+          src="/BIZ.png"
+          width={20}
+          height={20}
+          onClick={() => setCurrentDate(currentMonth.add(1, "month"))}
+          className="cursor-pointer"
+        />
+      </div>
+
+      <CalendarDiv>
+        {DaysOfWeek.map((day, index) => (
+          <span
+            style={{ fontSize: "14px", padding: "8px", width: "5%", textAlign: "center" }}
+            key={index}
+          >
+            {day}
+          </span>
+        ))}
+      </CalendarDiv>
+
+      <div>
+        {calendar.map((week, weekIndex) => (
+          <CalendarDiv key={weekIndex}>
+            {week.map((date, dayIndex) => (
+              <div
+                key={dayIndex}
+                style={{
+                  fontSize: "14px",
+                  padding: "8px",
+                  width: "5%",
+                  textAlign: "center",
+                  cursor: "pointer",
+                  backgroundColor: selectedDate?.isSame(date, "day") ? "#cce4ff" : "transparent",
+                }}
+                onClick={() => handleDateClick(date)}
+              >
+                {date.format("D")}
+              </div>
+            ))}
+          </CalendarDiv>
+        ))}
+      </div>
+    </CalendarContainer>
   );
 }
 
